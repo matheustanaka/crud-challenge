81b6653f5edee942b97ccb18bfc8b547013d58a9
matheustanaka <matheustanakanog@gmail.com>
2021-11-11 22:21:08 -0300

    feat: add user controller

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectToDatabase = require("./src/database/mongoose.db");
 
+dotenv.config();
 const app = express();
+app.use(express.json());
 connectToDatabase();
+
 app.get("/user", (request, response) => {
     return response.json({ message: "Hello World" });
 });
