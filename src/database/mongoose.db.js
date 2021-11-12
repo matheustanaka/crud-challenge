545d7919c8c02fb5ae75e6ba350b40dd2e0044ad
matheustanaka <matheustanakanog@gmail.com>
2021-11-11 21:25:07 -0300

    fix: database credentials

--- src/database/mongoose.db.js
+++ src/database/mongoose.db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
     await mongoose.connect(
-        "mongodb+srv://mvthexz:entria@cluster0.1fcuv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.1fcuv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
         () => console.log("Database is connected 😎")
     );
 };
