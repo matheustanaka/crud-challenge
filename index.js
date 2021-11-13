3ca2eafb6162d74d89a2cacbf438a90ce0ee2a6a
matheustanaka <matheustanakanog@gmail.com>
2021-11-12 21:03:55 -0300

    fix: catch error must be generics errors

--- index.js
+++ index.js
@@ -17,36 +17,43 @@ app.get("/users", async (req, res) => {
             return res.status(404).json({ error: "Not Found Users" });
         }
         //If users exists should return a status 200: The request succeeded
-        res.status(200).json(users);
+        return res.status(200).json(users);
     } catch (error) {
-        return res.status(404).json({ error: "Not Found Users" });
+        return res.status(500).json({ error: "Internal server error" });
     }
 });
 
 //Creating an user
 app.post("/users", async (req, res) => {
     try {
-        const newUser = new UserModel(req.body);
+        //Adding a new User, so here will receive a new UserModel(name,email,passoword)
+        const newUser = new UserModel(req.body); //req.body means that you will add some data inside your model
+        //Here you should wait, after, the newUser will be saved in database
         await newUser.save();
-
+        //send a 201 status: The request succeeded, new User was created.
         res.status(201).json(newUser);
     } catch (error) {
-        return res.status(500).json({ error: "You can't create an User" });
+        //send a 500 status: The server has encountered a situation it does not know how to handle.
+        return res.status(500).json({ error: "Internal server error" });
     }
 });
 
 //Getting an unique User by ID
 app.get("/users/:id", async (req, res) => {
     try {
+        //Requesting an id as paramater
         const userId = req.params.id;
+        //Waiting const user find the User ID inside our Model
         const user = await UserModel.findById(userId);
+        //If not found user Id return a 404 status: The server can not find the requested resource
         if (!user) {
             return res.status(404).json({ error: "Not Found User ID" });
         }
-
+        //return a 200 status: The requested succeeded
         return res.status(200).json(user);
     } catch (error) {
-        return res.status(500).json({ error: "Not Found User ID" });
+        //If not found user Id return a 404 status: The server can not find the requested resource
+        return res.status(500).json({ error: "Internal server error" });
     }
 });
 
@@ -79,7 +86,7 @@ app.put("/users/:id", async (req, res) => {
         return res.status(200).json(userToUpdate);
     } catch (error) {
         res.status(500).json({
-            error: "You can't update your user information",
+            error: "Internal server error",
         });
     }
 });
