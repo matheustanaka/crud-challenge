fc58db78f86d363a487f1cb71574f53e5ce05c49
matheustanaka <matheustanakanog@gmail.com>
2021-11-12 22:22:31 -0300

    feat: add the method to delete an user

--- index.js
+++ index.js
@@ -137,4 +137,24 @@ app.patch("/users/:id", async (req, res) => {
     }
 });
 
+app.delete("/users/:id", async (req, res) => {
+    try {
+        //Querying our ID
+        const userId = req.params.id;
+        //Finding user Id inside our model
+        const userToDelete = await UserModel.findById(userId);
+        //If there is not a Id return a 404 status as not found
+        if (!userToDelete) {
+            return res.status(404).json({ error: "Not Found User ID" });
+        }
+        //User deleted should wait the process of find an id and delete, passing user id as paramater
+        const userDeleted = await UserModel.findByIdAndDelete(userId);
+        //return a 200 status: The request was successfully
+        return res.status(200).json(userDeleted);
+    } catch (error) {
+        //return a generic error
+        return res.status(500).json({ error: "Internal server error" });
+    }
+});
+
 app.listen(3333, () => console.log("Listening on port 3333"));
