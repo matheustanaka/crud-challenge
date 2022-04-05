b827e30ef624ef6161c8ec3ae17beee86ddf4cdf
Matheus Tanaka <matheustanakanog@gmail.com>
2022-04-05 13:58:06 -0300

    fix: medicine models

--- src/models/medicine.model.js
+++ src/models/medicine.model.js
@@ -21,13 +21,10 @@ const MedicineSchema = Schema({
         type: String,
         required: true,
     },
-<<<<<<< HEAD
     user: {
         type: Schema.Types.ObjectId,
         ref: "User",
     },
-=======
->>>>>>> 0f28029 (docs: update README)
 });
 
 const MedicineModel = model("Medicine", MedicineSchema);
