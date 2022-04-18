e3974f1159385ea36ef301c8d9f033c795b7d6a8
Matheus Tanaka <matheustanakanog@gmail.com>
2022-04-17 22:46:21 -0300

    database: change my cluster

--- src/database/mongoose.db.js
+++ src/database/mongoose.db.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose");
 
 const connectToDatabase = async () => {
     await mongoose.connect(
-        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@crud-challenge.cmvjg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.cmvjg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
         () => console.log("Database is connected 😎")
     );
 };
 
 module.exports = connectToDatabase;
+
