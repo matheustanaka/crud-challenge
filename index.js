77fe6c60a1695cc4cf2b2a6683214c88cee73633
Matheus Tanaka <matheustanakanog@gmail.com>
2022-04-29 23:32:18 -0300

    refactor: change server port

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use("/users", UserRouter);
 //Defining our router as medicines
 app.use("/medicines", MedicineRouter);
 
-const serverPort = process.env.PORT || 8000;
+const serverPort = process.env.PORT || 8080;
 
 //Localhost port
 app.listen(serverPort, () => console.log(`Listening on port ${serverPort}`));
