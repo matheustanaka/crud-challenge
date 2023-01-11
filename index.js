88127f348826dbb93364f593e76ad2fe446fb96d
matheustanaka <matheustanakanog@gmail.com>
2023-01-11 19:18:11 -0300

    feat: change server port

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use("/users", UserRouter);
 //Defining our router as medicines
 app.use("/medicines", MedicineRouter);
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3333;
 
 //Localhost port
 app.listen(port, () => console.log(`Listening on port ${port}`));
